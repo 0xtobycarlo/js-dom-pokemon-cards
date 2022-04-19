4793e37bc59cad6f786ca1189911be02380e4a58
Toby Carlo <tobycarlo.c@gmail.com>
2022-04-19 16:07:00 +0100

    starting .js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
-console.log(data);
-
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+for (const element of data) {
+  const ulCards = document.querySelector("ul");
+  const liElement = document.createElement("li");
+  liElement.setAttribute("class", "card");
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "card--title");
+  const img = document.createElement("img");
+  img.setAttribute("class", "card--img");
+  img.setAttribute("width", "256");
+}
